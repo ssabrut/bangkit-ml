6ffe7e78ebc296ecd3d31c30e4d883c8b45546d1
ssabrut <michael.gunawan2002@gmail.com>
2023-06-08 17:02:42 +0700

    Fix response recommendation

--- bangkit-api/common/recommender_utils.py
+++ bangkit-api/common/recommender_utils.py
@@ -48,9 +48,8 @@ class RecommederModel:
 
     def pipeline(self):
         self.preprocess()
-        test_data = ' '.join(self.data.values)
+        test_data = ' '.join(self.data.values[0])
         vectorizer = TfidfVectorizer()
-        self.anchor_data = self.anchor_data.drop_duplicates(['plant_clean'])
         tfidf_matrix = vectorizer.fit_transform(self.anchor_data['feature'])
         tfidf = tfidf_matrix.toarray()
         plant_representation = self.model.predict(tfidf)
@@ -59,8 +58,8 @@ class RecommederModel:
         test_representation = self.model.predict(tfidf)
         similar_plants = cosine_similarity(test_representation, plant_representation)
         recommended_plant_index = tf.argmax(similar_plants)
-        recommended_plant = self.anchor_data.iloc[recommended_plant_index]['plant_clean'].values
-        return recommended_plant
+        recommended_plant = self.anchor_data.iloc[recommended_plant_index]['plant_clean'].drop_duplicates()
+        return recommended_plant.values[0].split()
 
 
 
